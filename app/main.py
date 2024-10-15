168f3f38ba37f8f92a4aa7559493a2c122998b95
Omerkhan23 <durraniaslankhan@gmail.com>
2024-10-15 11:45:39 +0500

    stage 2

--- app/main.py
+++ app/main.py
@@ -6,11 +6,11 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    
+    pong = "+PONG\r\n"
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client 
-    client , addr = server_socket.accept()
-    client.send(b"+PONG\r\n")
+     
+    client , addr = server_socket.accept()# wait for client
+    client.send(pong.encode())
 
 
 if __name__ == "__main__":
